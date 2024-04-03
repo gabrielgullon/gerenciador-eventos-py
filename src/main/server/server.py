129a3b9cde1c724d6d650d21ba983644bf18f25e
gabrielgullon <ggullong@gmail.com>
2024-04-03 18:17:11 -0300

    feat: create event api

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
+from src.models.settings.connections import db_connection_handler
+
+
+db_connection_handler.connect_to_db()
 
 app = Flask(__name__)
 CORS(app)
